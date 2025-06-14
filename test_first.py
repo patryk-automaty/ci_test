15224acd9a05a696c1fe78815f9c3c1ce235f580
patryk <patkycautomaty@gmail.com>
2025-06-14 13:32:39 +0000

    first commit

---
+++ test_first.py
@@ -0,0 +1,30 @@
+import random
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+
+def test_register():
+
+    chrome_options = ChromeOptions()
+
+
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
+
+
+    service = ChromeService(ChromeDriverManager().install())  # Use ChromeService
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+
+    # chrome_options: ChromeOptions = webdriver.ChromeOptions()
+    # chrome_options.add_argument("--headless")
+    # service_obj = Service("chromedriver")
+    random_email = str(random.randint(1,10000))
+    driver.get("http://seleniumdemo.com/?page_id=7")
+    driver.find_element(By.ID, "reg_email").send_keys("testmail" + random_email + "@test.com")
+    driver.find_element(By.ID, "reg_password").send_keys("testttt123!!!!!")
+    driver.find_element(By.NAME, "register").click()
